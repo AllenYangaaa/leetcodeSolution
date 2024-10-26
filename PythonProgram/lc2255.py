7424e579962eac863359bc96d52c1cfa47ed17ef
AllenYangaaa <yuanxiuy@usc.edu>
2024-10-26 11:47:30 +0800

    lc2255 finished

---
+++ PythonProgram/lc2255.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        ans = 0
+        for i in range(len(words)):
+            if self.is_prefix(words[i], s):
+                ans += 1
+        return ans
+    def is_prefix(self, a, b):
+        if len(b) < len(a):
+            return False
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return False
+        return True
+    '''
+    tc:O(m*n)
+    sc:O(1)
+    '''
+
+a = Solution()
+print (a.countPrefixes(["a","b","c","ab","bc","abc"], "abc"))
+          
